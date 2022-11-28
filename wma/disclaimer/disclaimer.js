9e3af261219db1b907ac15700aae5c1ead89d772
MAKIENAUT <mcrayescoto@gmail.com>
2022-11-28 23:16:46 +0800

    DISCLAIMER

--- wma/disclaimer/disclaimer.js
+++
@@ -1,6 +0,0 @@
-function copyToClipboard() {
-  navigator.clipboard.writeText
-      ("admin@westmigrationagency.us");
-
-  alert("Admin Email Address copied to clipboard: \n admin@westmigrationagency.us" )
-}
