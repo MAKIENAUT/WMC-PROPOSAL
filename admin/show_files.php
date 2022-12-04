94b04735077eb7c700de875e022f244c798ea3f0
MAKIENAUT <mcrayescoto@gmail.com>
2022-12-04 19:21:33 +0800

    PUSHED: FILE MANAGER

--- admin/show_files.php
+++ admin/show_files.php
@@ -45,6 +45,8 @@ if (!isset($_SESSION['loggedin'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="welcome.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
     <title>Document</title>
 </head>
 <body>
