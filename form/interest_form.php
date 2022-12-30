4254ccb299a97fab2c6c0f5cf7cc22b6a9f9f733
MAKIENAUT <mcrayescoto@gmail.com>
2022-12-30 14:38:31 +0800

    PUSHED: VALIDATION RESTRUCTING

--- form/interest_form.php
+++ form/interest_form.php
@@ -1,56 +1,55 @@
-<!-- <?php
-      include 'database.php';
-      $id = 0;
-      $lastname = "";
-      $firstname = "";
-      $address = "";
-      $country = "";
-      $state = "";
-      $phone_number = "";
-      $back_up = "";
-      $email = "";
-      $profession = "";
-      $file;
+<?php
+   include 'database.php';
+   $id = 0;
+   $lastname = "";
+   $firstname = "";
+   $address = "";
+   $country = "";
+   $state = "";
+   $phone_number = "";
+   $back_up = "";
+   $email = "";
+   $profession = "";
+   $file;
 
 
-      if (isset($_POST['submit'])) {
+   if (isset($_POST['submit'])) {
+      $lastname = $_POST['lastname'];
+      $firstname = $_POST['firstname'];
+      $address = $_POST['address'];
+      $country = $_POST['country'];
+      $state = $_POST['state'];
+      $phone_number = $_POST['phone_number'];
+      $back_up = $_POST['back_up'];
+      $email = $_POST['email'];
+      $profession = $_POST['profession'];
 
-         $lastname = $_POST['lastname'];
-         $firstname = $_POST['firstname'];
-         $address = $_POST['address'];
-         $country = $_POST['country'];
-         $state = $_POST['state'];
-         $phone_number = $_POST['phone_number'];
-         $back_up = $_POST['back_up'];
-         $email = $_POST['email'];
-         $profession = $_POST['profession'];
-
-         $file = $_FILES['file']['name'];
-         $file_tmp = $_FILES['file']['tmp_name'];
-         $foldername = '../admin/applicant_files/' . $_POST['lastname'] . ", " . $_POST['firstname'];
+      $file = $_FILES['file']['name'];
+      $file_tmp = $_FILES['file']['tmp_name'];
+      $foldername = '../admin/applicant_files/' . $_POST['lastname'] . ", " . $_POST['firstname'];
 
 
-         $sql = "INSERT INTO form VALUES (NULL, '$lastname', '$firstname', '$address', '$country', '$state', '$phone_number', '$back_up', '$email', '$profession', '$foldername')";
-         if (mysqli_query($conn, $sql)) {
-            if (!file_exists($foldername)) {
-               mkdir($foldername, 0777, true);
-               if (count($file) > 1) {
-                  for ($i = 0; $i < count($_FILES["file"]['name']); $i++) {
-                     $filen = $_FILES["file"]['name']["$i"]; //file name
-                     $path = $foldername . "/" . $filen; //generate the destination path
-                     if (move_uploaded_file($_FILES["file"]['tmp_name']["$i"], $path)) {
-                        echo "<script>alert('Success!')</script>";
-                     }
+      $sql = "INSERT INTO form VALUES (NULL, '$lastname', '$firstname', '$address', '$country', '$state', '$phone_number', '$back_up', '$email', '$profession', '$foldername')";
+      if (mysqli_query($conn, $sql)) {
+         if (!file_exists($foldername)) {
+            mkdir($foldername, 0777, true);
+            if (count($file) > 1) {
+               for ($i = 0; $i < count($_FILES["file"]['name']); $i++) {
+                  $filen = $_FILES["file"]['name']["$i"]; //file name
+                  $path = $foldername . "/" . $filen; //generate the destination path
+                  if (move_uploaded_file($_FILES["file"]['tmp_name']["$i"], $path)) {
+                     echo "<script>alert('Success!')</script>";
                   }
-               } else {
-                  echo "<script>alert('Upload the Necessary files!')</script>";
                }
             } else {
-               echo "<script>alert('INVALID!')</script>";
+               echo "<script>alert('Upload the Necessary files!')</script>";
             }
+         } else {
+            echo "<script>alert('INVALID!')</script>";
          }
       }
-      ?> -->
+   }
+   ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -59,7 +58,9 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+      integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+      crossorigin="anonymous" referrerpolicy="no-referrer" />
    <link rel="stylesheet" href="index.css">
    <script src="index.js"></script>
    <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
@@ -116,28 +117,36 @@
             <div class="personal_tab" id="personal_tab">
                <label id="personal-info">1. Personal Info *</label>
                <label id="name-title">Name *</label>
-               <input type="text" id="lastname" name="lastname" oninput="validate()" onclick="validate()" placeholder="Last Name">
-               <input type="text" id="firstname" name="firstname" oninput="validate()" onclick="validate()" placeholder="First Name">
+               <input type="text" id="lastname" name="lastname" onchange="validate()" onclick="validate()"
+                  placeholder="Last Name">
+               <input type="text" id="firstname" name="firstname" onchange="validate()" onclick="validate()"
+                  placeholder="First Name">
 
                <label id="address-title">Address *</label>
-               <input type="text" id="address" name="address" oninput="validate()" onclick="validate()" placeholder="Address">
-               <input type="text" id="country" name="country" oninput="validate()" onclick="validate()" placeholder="Country">
-               <input type="text" id="state" name="state" oninput="validate()" onclick="validate()" placeholder="State">
+               <input type="text" id="address" name="address" onchange="validate()" onclick="validate()"
+                  placeholder="Address">
+               <input type="text" id="country" name="country" onchange="validate()" onclick="validate()"
+                  placeholder="Country">
+               <input type="text" id="state" name="state" onchange="validate()" onclick="validate()" placeholder="State">
             </div>
 
             <div class="contact_tab" id="contact_tab">
                <label id="contact-info">2. Contact Info *</label>
                <label id="contact-title">Contacts *</label>
-               <input type="text" id="phone_number" name="phone_number" oninput="validate()" onclick="validate()" placeholder="Phone Number">
-               <input type="text" id="back_up" name="back_up" oninput="validate()" onclick="validate()" placeholder="BackUp Number" value="BackUp #: Edit if available.">
-               <input type="email" id="email" name="email" oninput="validate()" onclick="validate()" placeholder="Email Address" required>
+               <input type="text" id="phone_number" name="phone_number" onchange="validate()" onclick="validate()"
+                  placeholder="Phone Number">
+               <input type="text" id="back_up" name="back_up" onchange="validate()" onclick="validate()"
+                  placeholder="BackUp Number" value="BackUp #: Edit if available.">
+               <input type="email" id="email" name="email" onchange="validate()" onclick="validate()"
+                  placeholder="Email Address" required>
             </div>
             <div class="interest_tab" id="interest_tab">
                <label id="contact-info">3. Interest Form *</label>
                <label id="contact-title">Current Profession *</label>
 
                <div class="profession">
-                  <select id="profession" name="profession" onclick="validate()" onchange="showfield(this.options[this.selectedIndex].value)">
+                  <select id="profession" name="profession" onclick="validate()"
+                     onchange="showfield(this.options[this.selectedIndex].value)">
                      <option value="" disabled selected hidden>Choose Profession</option>
                      <option value="Teacher">Teacher</option>
                      <option value="Physician">Physician</option>
@@ -150,7 +159,8 @@
                      <option value="Intern">Intern</option>
                      <option value="Other" id="other">Other</option>
                   </select>
-                  <div id="div1">If Other: <input type="text" name="other_option" id="other_option" onclick="change()" oninput="change()" />
+                  <div id="div1">If Other: <input type="text" name="other_option" id="other_option" onclick="change()"
+                        onchange="change()" />
                   </div>
                </div>
 
@@ -161,7 +171,7 @@
                      Press "Yes" to proceed.
                   </label>
                   <div class="yes">
-                     <input type="radio" id="yes" name="eligible" value="Yes" onclick="termsAgree()" disabled>
+                     <input type="radio" id="yes" name="eligible" value="Yes" onchange="submitActivator()" onclick="termsAgree()" disabled>
                      <label for="yes" id="yes_label">YES - If unclickable, please check form.</label>
                   </div>
                   <div class="no">
@@ -173,6 +183,7 @@
                <div class="reference">
                   <p>Program Category Requirements</p>
                   <img class="reference_content" src="../photos/category.jpg">
+                  <img class="reference_content" src="../photos/category2.PNG">
                </div>
             </div>
             <div class="upload_tab">
@@ -209,32 +220,33 @@
                </div>
             </div>
             <div class="privacy_tab">
-               <label id="contact-info">5. Set an Appointment *</label>
-               <label id="contact-title">Remember to set an appointment first before proceeding to submit your form. *</label>
-               <!-- Calendly inline widget begin -->
-               <div class="calendly-inline-widget" data-url="https://calendly.com/mcrayescoto/wma-apointment?background_color=ddf8f8&text_color=191513&primary_color=008080" style="min-width:320px;height:630px;">
-               </div>
-            </div>
-            <div class="privacy_tab">
-               <label id="contact-info">6. Privacy Policy *</label>
-
+               <label id="contact-info">5. Privacy Policy *</label>
                <p>
-                  Read the Privacy Policy and give permission for West Migration Agency LLC to share my information and discuss my candidacy with any US Sponsors as part of the visa process.
+                  Read the Privacy Policy and give permission for West Migration Agency LLC to share my information and
+                  discuss my candidacy with any US Sponsors as part of the visa process.
                </p>
 
                <label id="contact-title">Terms and Conditions *</label>
-               <a href="https://docs.google.com/document/d/1WMAgSCGvUyVnzKEelKPdI4RcKVJ5v4KCrGwe4w60PNk/edit">Click And Read!</a>
+               <a href="https://docs.google.com/document/d/1WMAgSCGvUyVnzKEelKPdI4RcKVJ5v4KCrGwe4w60PNk/edit">Click And
+                  Read!</a>
 
                <div class="terms">
-                  <input type="checkbox" id="terms" name="terms" onclick="termsAgree(); submitActivator()">
+                  <input 
+                     type="checkbox" 
+                     id="terms" 
+                     name="terms" 
+                     onchange="validate()"
+                     onclick="termsAgree(); submitActivator()">
                   <label> Terms and Conditions *</label>
                </div>
 
                <div class="submit">
-                  <label onmouseover="submitActivator()" for="submit" id="validity_check">If submit button is still disabled, Please check the form's validity!</label>
+                  <label onmouseover="submitActivator()" for="submit" id="validity_check">If submit button is still
+                     disabled, Please check the form's validity!</label>
                   <br>
                   <br>
-                  <button type="submit" name="submit" id="submit" disabled onmouseover="submitActivator()">SUBMIT</button>
+                  <button type="submit" name="submit" id="submit" disabled
+                     onmouseover="submitActivator()">SUBMIT</button>
                </div>
             </div>
          </form>
